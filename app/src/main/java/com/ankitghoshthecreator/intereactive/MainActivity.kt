6ab9fe073da5b8fc4d995f39552d6ab83caef82f
ankitghoshthecreator <baruipara01@gmail.com>
2024-12-10 01:26:38 +0530

    themes dark and light buttons created

---
+++ app/src/main/java/com/ankitghoshthecreator/intereactive/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.ankitghoshthecreator.intereactive
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val darkTheme= findViewById<Button>(R.id.btnDark)
+        val lightTheme=findViewById<Button>(R.id.btnLight)
+
+        darkTheme.setOnClickListener {  }
+
+        lightTheme.setOnClickListener {  }
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
